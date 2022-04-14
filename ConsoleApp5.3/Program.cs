4d286707cf12a56ca22cfb2401c7b31484d7ed4f
Alex Zverev <lordzver007@yandex.ru>
2022-04-14 04:56:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5.3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp5._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWorking = true;
+
+            int number;
+
+            List<int> numbers = new List<int>();
+
+            while (isWorking)
+            {
+                var userInput = Console.ReadLine();
+                
+                if (userInput == "exit")
+                {
+                    isWorking = false;
+                    break;
+                }
+
+                else if (userInput == "sum")
+                {
+                    int sum = 0;
+
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        sum += numbers[i];
+                    }
+
+                    Console.WriteLine("Сумма всех чисел в массиве : " + sum);
+
+                    break;
+                }
+                else if(Int32.TryParse(userInput, out number))
+                {                    
+                    numbers.Add(number);
+                    Console.WriteLine($"Число {number} добавлено\n");
+                }
+                else
+                {
+                    Console.WriteLine($"Невозможно преобразовать {userInput} в число");
+                   
+                }
+            }
+        }
+    }
+}
